9d1dae4d5956de80636c9ba3056e20b5e23d7c4c
Enmanuel Ramirez <enmanuel.ramirez@reign.cl>
2020-11-21 23:26:54 -0300

    signup component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Signup from "./components/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup></Signup>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
