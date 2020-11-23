68fdeda9a77d61950bebd5a70d03cf6bbef92dfc
Enmanuel Ramirez <enmanuel.ramirez@reign.cl>
2020-11-22 21:05:58 -0300

    config env

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ForgotPassword from './components/ForgotPassword'
 import UpdateProfile from "./components/UpdateProfile";
 
 function App() {
+  console.log(process.env.NODE_ENV)
   return (
     <Container
     className="d-flex align-items-center justify-content-center"
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,33 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app'
 import 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAy013NRWxoX7A1Kq2y86TL0j_HK-MXa-g",
-  authDomain: "auth-react-51ba7.firebaseapp.com",
-  databaseURL: "https://auth-react-51ba7.firebaseio.com",
-  projectId: "auth-react-51ba7",
-  storageBucket: "auth-react-51ba7.appspot.com",
-  messagingSenderId: "712820859690",
-  appId: "1:712820859690:web:a2e684564df32dae647453",
-  measurementId: "G-3C4376CMKN",
-};
+let firebaseConfig
+
+if (process.env.NODE_ENV === 'production') {
+  firebaseConfig = {
+    apiKey: "AIzaSyAy013NRWxoX7A1Kq2y86TL0j_HK-MXa-g",
+    authDomain: "auth-react-51ba7.firebaseapp.com",
+    databaseURL: "https://auth-react-51ba7.firebaseio.com",
+    projectId: "auth-react-51ba7",
+    storageBucket: "auth-react-51ba7.appspot.com",
+    messagingSenderId: "712820859690",
+    appId: "1:712820859690:web:a2e684564df32dae647453",
+    measurementId: "G-3C4376CMKN",
+  };
+
+  firebase.analytics();
+} else if (process.env.NODE_ENV === 'development') {
+  firebaseConfig = {
+    apiKey: "AIzaSyAq6_ruQsjYPJTVbSOTq15q_PW6N0cmLU8",
+    authDomain: "auth-react-dev-bd9fe.firebaseapp.com",
+    databaseURL: "https://auth-react-dev-bd9fe.firebaseio.com",
+    projectId: "auth-react-dev-bd9fe",
+    storageBucket: "auth-react-dev-bd9fe.appspot.com",
+    messagingSenderId: "864453277938",
+    appId: "1:864453277938:web:dfa6fae63120b5fe61386b"
+  };
+}
 
 const app = firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 export default app;
 export const auth = firebase.auth();
